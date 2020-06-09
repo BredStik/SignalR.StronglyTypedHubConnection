478e71a46879304b5abf1248f9fc476d4a1830dc
BredStik <mathieulafontaine@hotmail.com>
2020-06-09 10:26:56 -0400

    initial commit

---
+++ SignalR.StronglyTypedHubConnection/SignalR.StronglyTypedHubConnection/HubConnectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalR.StronglyTypedHubConnection
+{
+    public static class HubConnectionExtensions
+    {
+        public static async Task InvokeAsync<THubType>(this HubConnection hubConnection, Expression<Action<THubType>> call, CancellationToken cancellationToken)
+        {
+            var methodCall = (MethodCallExpression)call.Body;
+            var methodName = methodCall.Method.Name;
+            var arguments = methodCall.Arguments.Select(GetValue).ToArray();
+
+            await hubConnection.InvokeCoreAsync(methodName, arguments, cancellationToken).ConfigureAwait(false);
+        }
+
+        public static async Task<TResult> InvokeAsync<THubType, TResult>(this HubConnection hubConnection, Expression<Func<THubType, TResult>> call, CancellationToken cancellationToken)
+        {
+            var methodCall = (MethodCallExpression)call.Body;
+            var methodName = methodCall.Method.Name;
+            var arguments = methodCall.Arguments.Select(GetValue).ToArray();
+
+            return (TResult)await hubConnection.InvokeCoreAsync(methodName, typeof(TResult), arguments, cancellationToken).ConfigureAwait(false);
+        }
+
+        private static object GetValue(Expression valueExpression)
+        {
+            var objectMember = Expression.Convert(valueExpression, typeof(object));
+            var getterLambda = Expression.Lambda<Func<object>>(objectMember);
+            return getterLambda.Compile()();
+        }
+    }
+}
